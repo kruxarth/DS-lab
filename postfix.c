4f9e1a2319afad0bacbdb8316f2865dcac4cd306
kruxarth <kruxarth@gmail.com>
2025-09-29 14:53:42 +0530

    fix postfix

--- postfix.c
+++ postfix.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <ctype.h>   // for isdigit()
-#include <stdlib.h>  // for atoi()
+#include <ctype.h>  
+#include <stdlib.h>  
 
 #define MAX 100
 
-// Stack implementation
 int stack[MAX];
 int top = -1;
 
 void push(int x) {
     if (top == MAX - 1) {
         printf("Stack overflow\n");
-        return;
+        exit(1);
     }
     stack[++top] = x;
 }
@@ -19,31 +18,27 @@ void push(int x) {
 int pop() {
     if (top == -1) {
         printf("Stack underflow\n");
-        return -1;
+        exit(1);
     }
     return stack[top--];
 }
 
-// Function to evaluate postfix expression
 int evaluatePostfix(char* exp) {
     for (int i = 0; exp[i] != '\0'; i++) {
         char ch = exp[i];
 
         if (isdigit(ch)) {
-            // Convert char digit to int and push
             push(ch - '0');
-        }
-        else {
-            // Operator: pop two operands
-            int val2 = pop();
-            int val1 = pop();
+        } else {
+            int val1 = pop(); 
+            int val2 = pop(); 
 
             switch (ch) {
-                case '+': push(val1 + val2); break;
-                case '-': push(val1 - val2); break;
-                case '*': push(val1 * val2); break;
-                case '/': push(val1 / val2); break;
-                default: printf("Invalid operator: %c\n", ch);
+                case '+': push(val2 + val1); break;
+                case '-': push(val2 - val1); break;
+                case '*': push(val2 * val1); break;
+                case '/': push(val2 / val1); break;
+                default: printf("Invalid operator: %c\n", ch); exit(1);
             }
         }
     }
